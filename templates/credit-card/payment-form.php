f2fb6547e13bbdeea8cee7165b38b2bdcaccde7f
Claudio Sanches <contato@claudiosmweb.com>
2015-05-01 01:23:15 -0300

    Improved the credit card payment form

--- templates/credit-card/payment-form.php
+++ templates/credit-card/payment-form.php
@@ -14,12 +14,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <fieldset id="iugu-credit-card-fields">
 	<p class="form-row form-row-first">
-		<label for="iugu-card-holder-name"><?php _e( 'Card Holder Name', 'iugu-woocommerce' ); ?> <small>(<?php _e( 'as recorded on the card', 'iugu-woocommerce' ); ?>)</small> <span class="required">*</span></label>
-		<input id="iugu-card-holder-name" name="iugu_card_holder_name" class="input-text" type="text" autocomplete="off" style="font-size: 1.5em; padding: 8px;" data-iugu="full_name" />
-	</p>
-	<p class="form-row form-row-last">
 		<label for="iugu-card-number"><?php _e( 'Card Number', 'iugu-woocommerce' ); ?> <span class="required">*</span></label>
 		<input id="iugu-card-number" class="input-text wc-credit-card-form-card-number" type="text" maxlength="20" autocomplete="off" placeholder="&bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull;" style="font-size: 1.5em; padding: 8px;" data-iugu="number" />
+	</p>
+	<p class="form-row form-row-last">
+		<label for="iugu-card-holder-name"><?php _e( 'Name Printed on the Card', 'iugu-woocommerce' ); ?> <span class="required">*</span></label>
+		<input id="iugu-card-holder-name" name="iugu_card_holder_name" class="input-text" type="text" autocomplete="off" style="font-size: 1.5em; padding: 8px;" data-iugu="full_name" />
 	</p>
 	<div class="clear"></div>
 	<p class="form-row form-row-first">
