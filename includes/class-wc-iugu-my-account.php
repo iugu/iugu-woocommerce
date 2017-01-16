831069f32f8e37e826e2264c180c77b1a177f179
Alvaro Separovich <alvasepro@gmail.com>
2017-01-16 10:08:00 -0200

    grammar and translation fix

--- includes/class-wc-iugu-my-account.php
+++ includes/class-wc-iugu-my-account.php
@@ -13,13 +13,13 @@ class WC_Iugu_My_Account {
 	 * Initialize my account actions.
 	 */
 	public function __construct() {
-		add_filter( 'woocommerce_my_account_my_orders_actions', array( $this, 'my_orders_banking_ticket_link' ), 10, 2 );
+		add_filter( 'woocommerce_my_account_my_orders_actions', array( $this, 'my_orders_bank_slip_link' ), 10, 2 );
 	}
 
 	/**
-	 * Add banking ticket link/button in My Orders section on My Accout page.
+	 * Add bank slip link/button in My Orders section on My Accout page.
 	 */
-	public function my_orders_banking_ticket_link( $actions, $order ) {
+	public function my_orders_bank_slip_link( $actions, $order ) {
 		if ( 'iugu-bank-slip' !== $order->payment_method ) {
 			return $actions;
 		}
@@ -32,7 +32,7 @@ class WC_Iugu_My_Account {
 		if ( ! empty( $data['pdf'] ) ) {
 			$actions[] = array(
 				'url'  => $data['pdf'],
-				'name' => __( 'Print Banking Ticket', 'iugu-woocommerce' ),
+				'name' => __( 'Pay the bank slip', 'iugu-woocommerce' ),
 			);
 		}
 
